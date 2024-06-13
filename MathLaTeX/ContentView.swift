312406bfbc6563ac426628d351f2d264ead5e333
yanyanmaru <manmarutech@gmail.com>
2024-06-13 11:37:00 +0900

    Initial Commit

---
+++ MathLaTeX/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MathLaTeX
+//
+//  Created by 平山奈々海 on 2024/06/13.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MathLaTeX/MathLaTeXApp.swift
@@ -0,0 +1,17 @@
+//
+//  MathLaTeXApp.swift
+//  MathLaTeX
+//
+//  Created by 平山奈々海 on 2024/06/13.
+//
+
+import SwiftUI
+
+@main
+struct MathLaTeXApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
